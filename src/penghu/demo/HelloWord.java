e1385dd82adeb87196e6cd396e21bce38f63e70b
hu.peng <hu.peng@zhangmen.com>
2020-07-01 10:50:13 +0800

    '测试'

---
+++ src/penghu/demo/HelloWord.java
@@ -0,0 +1,11 @@
+package penghu.demo;
+
+/**
+ * @author hu.peng
+ * @creat 2020-06-30-16:14
+ */
+public class HelloWord {
+    public static void main( String[] args) {
+        System.out.println("hello world!");
+    }
+}
