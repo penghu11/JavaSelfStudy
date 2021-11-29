eb90703299cbf189e11347724402670eb0c9b8c4
ph <1187018985@qq.com>
2021-11-29 21:37:13 +0800

    Update HelloWord.java

--- src/penghu/demo/HelloWord.java
+++ src/penghu/demo/HelloWord.java
@@ -1,9 +1,4 @@
-package penghu.demo;
 
-/**
- * @author hu.peng
- * @creat 2020-06-30-16:14
- */
 public class HelloWord {
     public static void main( String[] args) {
         System.out.println("hello world!");
